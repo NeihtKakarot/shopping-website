ad8d0ee64dc4826bc29b83b4babe0e8453ae878c
Neiht <jinmoriazaaa@gmail.com>
2024-07-16 10:20:27 +0700

    Making my first commit

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,12 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.rtvgqfj.mongodb.net',
+    DB_USER: 'Neiht',
+    DB_PASS: '123456XD',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '71be694fe29e1e2c790f6e006852590f2c10f665186175acd8331b41f0d3eac7c9f7cabbb23a75ab60093a276bea637f0de463dabb77bd86d6159446975b5df7c5eef5aee2ecdb79bec529185e2a39ab48b2e176052106128b936b56dd05cffa48abac0fa8d3b2c8147846214d575fdd0754968d83a66e198437858271b2c792f0519590885de0b511686127e3d31fc6fed7894bcbf59594e5f29c68a75cb3136dd2b84e384d49c319bb22cd65433cb960e04c638ec8bc837c1b72ce0181c6b9422c0f504c95eb683b5c58f4c75b60cdc8a4faf4f40cdaa8006b7e3fc821bc657aa746f5f06fa0b303beeb834089237980f43752c6af46798dac5c54d05547c4',
+    JWT_EXPIRES: '3600000', // in milliseconds
+    EMAIL_USER: 't9826259@gmail.com', // gmail service
+    EMAIL_PASS: 'shoppingonline1'
+  };
+  module.exports = MyConstants;
+
